fc4fad4abfa8e12370f8817e7fdb2529186e6237
Aryan Chaudhary <aryannov25@gmail.com>
2023-01-26 12:30:09 +0530

    update

--- src/Pages/NewPost.js
+++ src/Pages/NewPost.js
@@ -44,60 +44,60 @@ function Home() {
   return (
     <div className="bg">
       <Nav />
-<section className="">
-      <div className="container1 text-center mt-1">
-            
-      <h1 className="px-0 py-2 text-2xl font-bold text-white">
-                  Hello {user.email}
-                </h1>
+      <section className="">
+        <div className="container1 text-center mt-1">
+          <h1 className="px-0 py-2 text-2xl font-bold text-white">
+            Hello {user.email}
+          </h1>
 
-                <h3 className="text-white">
-                  How are you doing today? Would you like to share something
-                  with the <br />
-                  community &#129303;
-                </h3>
+          <h3 className="text-white">
+            How are you doing today? Would you like to share something with the{" "}
+            <br />
+            community &#129303;
+          </h3>
 
-                <form className="flex justify-center items-center h-96  mt-4 " onSubmit={submitHandler}>
-              <div className="form-post rounded">
-                {/* <!--middle creat tweet--> */}
-                <h2 className="px-4 py-3 text-lg font-semibold text-white">
-                  Create Post
-                </h2>
+          <form
+            className="flex justify-center items-center h-96  mt-4 "
+            onSubmit={submitHandler}
+          >
+            <div className="form-post rounded">
+              {/* <!--middle creat tweet--> */}
+              <h2 className="px-4 py-3 text-lg font-semibold text-white">
+                Create Post
+              </h2>
 
-                <div className="flex rounded mx-4 form-post">
-                  <div className="m-2 w-10 py-1">
-                    <img
-                      className="inline-block h-10 w-10 rounded-full"
-                      src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
-                      alt=""
-                    />
-                  </div>
-                  <div className="flex-1 p-4  ">
-                    <textarea
-                      className=" bg-transparent text-white font-medium text-lg w-full"
-                      rows="2"
-                      cols="70"
-                      placeholder="What's happening?"
-                      ref={tweetInputRef}
-                    ></textarea>
-                  </div>
+              <div className="flex rounded mx-4 form-post">
+                <div className="m-2 w-10 py-1">
+                  <img
+                    className="inline-block h-10 w-10 rounded-full"
+                    src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+                    alt=""
+                  />
                 </div>
-                <div className="flex">
-                  <div className="flex-1 mb-2">
-                    <button
-                      type="submit"
-                      className="bg-blue-400 hover:bg-blue-500 mt-4 text-white font-bold py-2 px-8 rounded-full mr-8 float-right"
-                    >
-                      Post
-                    </button>
-                  </div>
+                <div className="flex-1 p-4  ">
+                  <textarea
+                    className=" bg-transparent text-white font-medium text-lg w-full"
+                    rows="2"
+                    cols="70"
+                    placeholder="What's happening?"
+                    ref={tweetInputRef}
+                  ></textarea>
                 </div>
               </div>
-            </form>
-
-
+              <div className="flex">
+                <div className="flex-1 mb-2">
+                  <button
+                    type="submit"
+                    className="bg-blue-400 hover:bg-blue-500 mt-4 text-white font-bold py-2 px-8 rounded-full mr-8 float-right"
+                  >
+                    Post
+                  </button>
+                </div>
+              </div>
             </div>
-</section>
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
