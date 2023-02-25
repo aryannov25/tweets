86e852c7357ae08e68e38d2091f58d2adce2d881
Aryan Chaudhary <aryannov25@gmail.com>
2023-02-25 20:31:43 +0530

    update

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "../App.css";
 import {
   Navbar,
